ab192cdc1aadf6fd4d702d6e54486bd3b08732ac
Juan Camilo Becerra Restrepo <camilo273@gmail.com>
2025-03-13 15:21:00 -0500

    Updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './platzi.webp';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'Estudiar', completed: false },
+];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo=>!!todo.completed).length;
+  const totalTodos = todos.length;
+  
+  const searchedTodos = todos.filter((todos) => {
+    const todoText = todos.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText)});
+  
+  console.log('Los usuarios buscan en la lista:' + searchValue);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue
+        }
+      />
+
+      <TodoList>
+        {searchedTodos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
+      </TodoList> 
+
+      <CreateTodoButton />
+    </>
   );
 }
+
+
+
 
 export default App;
